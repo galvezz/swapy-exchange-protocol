828a5b086aae64e47891acc9d3df8e4ce8aea9d1
Adriel <adrielgusa@gmail.com>
2017-12-22 20:18:20 -0200

    Testing token on testnet

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,9 +4,7 @@ let AssetLibrary = artifacts.require("./investment/AssetLibrary.sol");
 let Token = artifacts.require("./token/Token.sol");
 
 module.exports = function(deployer, network, accounts) {
-  deployer.deploy(AssetLibrary).then(() => {  
-    return deployer.deploy(Token).then(() => {
-      return deployer.deploy(SwapyExchange, AssetLibrary.address, Token.address);
-    })
+  deployer.deploy(AssetLibrary).then(() => {
+    return deployer.deploy(SwapyExchange, AssetLibrary.address, '0xddcc1ebf2f4d47b485a201b64f41c1ddd18ab247');
   })
 };
